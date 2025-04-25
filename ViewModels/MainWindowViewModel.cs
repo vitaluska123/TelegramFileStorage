32839e7566b65d5287c9f2410990984ef67eff00
strunder <me@strunder.ru>
2025-04-25 16:16:54 +0300

    feat: добавлен ViewModelBase и команды для кнопок действий в MainWindowViewModel, привязка команд в MainWindow.axaml

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,7 @@
 using System.Collections.ObjectModel;
 using TelegramFileStorage.Models;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
 
 namespace TelegramFileStorage.ViewModels
 {
@@ -7,6 +9,26 @@ namespace TelegramFileStorage.ViewModels
     {
         public ObservableCollection<FileModel> Files { get; set; } = new();
         public string CurrentPath { get; set; } = "/";
-        // ...добавить команды для скачивания, загрузки, перемещения, удаления, настроек
+
+        public ICommand DownloadCommand { get; }
+        public ICommand UploadCommand { get; }
+        public ICommand MoveCommand { get; }
+        public ICommand DeleteCommand { get; }
+        public ICommand SettingsCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            DownloadCommand = new RelayCommand(OnDownload);
+            UploadCommand = new RelayCommand(OnUpload);
+            MoveCommand = new RelayCommand(OnMove);
+            DeleteCommand = new RelayCommand(OnDelete);
+            SettingsCommand = new RelayCommand(OnSettings);
+        }
+
+        private void OnDownload() { /* TODO: Реализация скачивания */ }
+        private void OnUpload() { /* TODO: Реализация загрузки */ }
+        private void OnMove() { /* TODO: Реализация перемещения */ }
+        private void OnDelete() { /* TODO: Реализация удаления */ }
+        private void OnSettings() { /* TODO: Открытие настроек */ }
     }
 }
